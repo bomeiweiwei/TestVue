2f5196652baf158fbb936657b666cff63b8fe476
DESKTOP-R9CU97T\bomei <bomeilonch0210@gmail.com>
2020-09-30 21:54:07 +0800

    輪播圖

---
+++ Vue5/app.js
@@ -0,0 +1,13 @@
+var vm=new Vue({
+    el:'#app',
+    data:{
+        slideList: [
+            { src: './images/slide1.jpg', alt: 'a1' },
+            { src: './images/slide2.jpg', alt: 'a2' },
+            { src: './images/slide3.jpg', alt: 'a3' }
+        ]
+    },
+    methods:{
+
+    }
+});
